f305c7f241e75c45326cc9fcc403101a509120f0
Yarochkin <ya.yarochkin@yandex.ru>
2025-05-23 14:01:39 +0300

    added cli 1

--- anki_export.py
+++ anki_export.py
@@ -10,6 +10,21 @@ def get_definition_and_example(word):
     examples = synsets[0].examples()
     example = examples[0] if examples else ""
     return (definition, example)
+from nltk.corpus import wordnet as wn
+
+def get_better_definition(word):
+    synsets = wn.synsets(word)
+    if not synsets:
+        return "", ""
+
+    # Prefer nouns > verbs > others
+    priority = ['n', 'v', 'a', 'r']
+    synsets.sort(key=lambda s: priority.index(s.pos()) if s.pos() in priority else 99)
+
+    definition = synsets[0].definition()
+    example = synsets[0].examples()[0] if synsets[0].examples() else ""
+    return definition, example
+
 
 def get_phonetic_placeholder(word):
     # Placeholder phonetics (IPA data requires external services or CMU dict)
